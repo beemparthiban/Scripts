e9b93256df2896dbe61061f71ab6be164d0633f1
beemparthiban <beemparthiban@gmail.com>
2019-03-26 21:08:10 +0000

    Update whoisxmlapi.py

--- whoisxmlapi.py
+++ whoisxmlapi.py
@@ -1,28 +1,43 @@
 import requests
 from bs4 import BeautifulSoup
 import urllib.request
+import time
+from prettytable import PrettyTable
+
+start = time.time()
+
+api_key = 'your_api_key'
+
+url_1 = 'https://www.whoisxmlapi.com/whoisserver/WhoisService'
+
+#to store the final URL's to be queried against whoisxml
+api_urls = []
 
 
-api_address = 'https://www.whoisxmlapi.com/whoisserver/WhoisService?apiKey='remove_quotes_and_put_your_api_key_here'&outputFormat=XML&domainName='
-domain = input("enter the domain name: ")
-#url_list= []
-final_url = api_address+domain
-print(final_url)
-#source = requests.get(final_url)
+#creating a table to display the output
+table = PrettyTable()
+table.field_names = ["Domain_Name", "Domain_Age"]
+
+#get the list of inouts from user and store it as a list
+user_input = input("Enter the list of domains separated by commas: ")
+input_list = user_input.split(',')
+numbers = [str(x.strip()) for x in input_list]
+
+#form the URL's to be queried. place the user entered domains
+for domain in numbers:
+    final_url = url_1  + '?domainName=' + domain + '&apiKey=' + api_key + '&outputFormat=JSON'
+    api_urls.append(final_url)
+
+#get the list of URL's from the list and query against the WHOISXML
+for words in api_urls:
+    json_data = requests.get(words).json()
+    domain = json_data['WhoisRecord']['domainName']
+    age = json_data['WhoisRecord']['estimatedDomainAge']
+    print(domain,age)
+    table.add_row([domain, age])
+
+print(str(table) )
 
 
-source = urllib.request.urlopen(final_url).read()
-
-soup = BeautifulSoup(source,'xml')
-
-
-for data in soup:
-    try:
-        age = data.find('estimatedDomainAge').text
-        email = data.find('contactEmail').text
-        domain = data.find('domainName').text
-        print("Domain Name: ", domain)
-        print("Contact Email: ", email)
-        print("Domain Age: ", age)
-    except Exception:
-        pass
+end = time.time()
+print("Total time taken to execute the script: ", end - start,"secs")
