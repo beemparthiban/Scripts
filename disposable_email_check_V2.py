3372a47177fff2a7fa113ed113262809a1a6bec4
beemparthiban <beemparthiban@gmail.com>
2019-03-29 12:13:30 +0000

    Create disposable_email_check_V2.py

---
+++ disposable_email_check_V2.py
@@ -0,0 +1,33 @@
+import json
+import requests
+from prettytable import PrettyTable
+
+url = 'https://open.kickbox.com/v1/disposable/'
+
+# creating a table to display the output
+table = PrettyTable()
+table.field_names = ["Email", "Result"]
+
+# get the list of inputs from user and store it as a list
+user_input = input("Enter the list of email addresses separated by commas: ")
+input_list = user_input.split(',')
+emails = [str(x.strip()) for x in input_list]
+
+# to store the final URL's to be queried against kickbox
+api_urls = [url + email for email in emails]
+
+# get the list of URL's from the list and query against the kickbox
+results = [requests.get(url).json().get('disposable') for url in api_urls]
+
+# using another for loop to print the result and corresponding domain
+for email, result in zip(emails, results):
+    table.add_row([email, result])   # adding result to the table
+    #print(z,result)
+
+print()
+print ("Free email stats: \n ")
+print("True -> It's a disposable email address")
+print("False -> Not a disposable address")
+print("\n")
+
+print(table)
